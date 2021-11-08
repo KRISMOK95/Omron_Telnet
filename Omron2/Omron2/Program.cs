a66ad7ce8bd559a2f8a39fa9912a1415e4b4a890
KRISMOK95 <71318548+KRISMOK95@users.noreply.github.com>
2021-11-08 19:15:21 +0100

    Add files via upload

---
+++ Omron2/Omron2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace Omron2
+{
+   
+        class Program
+        {
+
+            static void Main(string[] args)
+            {
+            Console.WriteLine("Hello World!");
+            telnet2 tel = new telnet2();
+            tel.EstablishConnection("192.168.11.203", 7171);
+            Console.WriteLine("Connected!");
+            System.Threading.Thread.Sleep(1000);
+
+            
+            }
+        }
+    
+}
---
+++ Omron2/Omron2/telnet2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Omron2
+{
+    class telnet2
+    {
+        public TcpClient client;
+        private NetworkStream stream;
+        public telnet2()
+        {
+            Console.WriteLine("Oject created");
+        }
+
+
+        public void EstablishConnection(string ip_address, int port_number)
+        {
+            try
+            {
+                client = new TcpClient(ip_address, port_number); //thats a comment
+                Console.WriteLine("Client created");
+#if DEBUG
+
+                Console.WriteLine("[Communication] : [EstablishConnection] : Success connecting to : {0}, port: {1}", ip_address, port_number);
+#endif
+                Console.WriteLine("Stream created");
+                Console.Write("Enter the string to be transmitted : ");
+                String str = Console.ReadLine();
+
+                stream = client.GetStream();
+                ASCIIEncoding asen = new ASCIIEncoding();
+                byte[] ba = asen.GetBytes(str);
+                Console.WriteLine("Transmitting.....");
+                stream.Write(ba, 0, ba.Length);
+
+                byte[] bb = new byte[100];
+                int k = stream.Read(bb, 0, 100);
+
+                for (int i = 0; i < k; i++)
+                    Console.Write(Convert.ToChar(bb[i]));
+
+                //client.Close();
+            }
+            catch
+            {
+                Console.WriteLine("[Communication] : [EstablishConnection] : Failed while connecting to : {0}, port: {1}", ip_address, port_number);
+                System.Environment.Exit(1);
+            }
+        }
+
+    }
+}
